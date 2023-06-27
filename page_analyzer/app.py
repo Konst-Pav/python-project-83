78429df46f030584c4e5607927f12c6357985cd1
Konstantin Pavlov <k.pavlov.reg@yandex.ru>
2023-06-27 17:17:40 +0300

    first commit

---
+++ page_analyzer/app.py
@@ -0,0 +1,18 @@
+from flask import (
+    Flask,
+    flash,
+    get_flashed_messages,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, hello!'
