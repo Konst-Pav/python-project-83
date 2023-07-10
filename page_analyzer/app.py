da3d36e79b43c881c5fd8ab98f1586e5ef396c4f
Konstantin Pavlov <k.pavlov.reg@yandex.ru>
2023-07-10 10:52:42 +0300

    update

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,6 +8,8 @@ from flask import (
     request,
     url_for,
 )
+from validators.url import url as validate_url
+import psycopg2
 
 
 app = Flask(__name__)
@@ -16,3 +18,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.post('/urls')
+def post_urls():
+    return redirect(url_for(''))
+
+
+@app.route('/urls/<id>')
+def get_url(id):
+    return render_template('url_page.html')
