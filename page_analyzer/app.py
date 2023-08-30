98439b666aa2c0c891c5414bfce63d17093a31dc
Konstantin Pavlov <k.pavlov.reg@yandex.ru>
2023-08-30 12:49:01 +0300

    refactoring

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -30,13 +30,8 @@ def index():
     return render_template('index.html', url='', messages=messages)
 
 
-# @app.get('/urls')
-# def get_urls():
-#
-# @app.post('/urls')
-# def post_urls():
 @app.route('/urls', methods=['GET', 'POST'])
-def post_urls():
+def urls():
     url_from_user = request.form.get('url', '')
     url_from_user = url_from_user.lower()
     if request.method == 'POST':
@@ -50,23 +45,22 @@ def post_urls():
         url_already_in_db = db.get_data_by_url_name(name)
         if url_already_in_db:
             flash('Страница уже существует', 'alert alert-info')
-            return redirect(url_for('get_url', id=url_already_in_db['id']))
+            return redirect(url_for('get_url_page', id=url_already_in_db['id']))
         db.add_url(name)
         url_data = db.get_data_by_url_name(name)
         flash('Страница успешно добавлена', 'alert alert-success')
-        return redirect(url_for('get_url', id=url_data.get('id')))
+        return redirect(url_for('get_url_page', id=url_data.get('id')))
     else:
         messages = get_flashed_messages(with_categories=True)
-        urls = db.get_urls_data()
-        for url in urls:
+        urls_data = db.get_urls_data()
+        for url in urls_data:
             if url.get('created_at'):
                 url['created_at'] = datetime.date(url['created_at'])
-        return render_template('urls.html', urls=urls, messages=messages)
+        return render_template('urls.html', urls=urls_data, messages=messages)
 
 
-# get_url_page(id)
 @app.route('/urls/<int:id>')
-def get_url(id):
+def get_url_page(id):
     url_data = db.get_data_by_url_id(id)
     url_data['created_at'] = datetime.date(url_data.get('created_at', ''))
     urls_check_list = db.get_url_checks_by_url_id(id)
@@ -76,7 +70,6 @@ def get_url(id):
     return render_template('url_page.html', messages=messages, url=url_data, urls_check_list=urls_check_list)
 
 
-# post_url_check(id)
 @app.post('/urls/<int:id>/checks')
 def post_url_check(id):
     url = db.get_data_by_url_id(id).get('name')
@@ -88,6 +81,6 @@ def post_url_check(id):
         description = url_data.get('description', '')
         db.add_url_check(id, status_code=status_code, h1=h1, title=title, description=description)
         flash('Страница успешно проверена', 'alert alert-success')
-        return redirect(url_for('get_url', id=id))
+        return redirect(url_for('get_url_page', id=id))
     flash('Произошла ошибка при проверке', 'alert alert-danger')
-    return redirect(url_for('get_url', id=id))
+    return redirect(url_for('get_url_page', id=id))
